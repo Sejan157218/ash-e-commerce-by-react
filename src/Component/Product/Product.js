4b61aa8b04151eff6a6bf5e95a2535fd2c380478
sejan <sejan157218@gmail.com>
2021-09-25 13:50:13 +0600

    add dynamc star

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import Rating from 'react-rating';
 
 const Product = (props) => {
     
-    const {name,img,price,seller,stock} = props.product;
+    const {name,img,price,seller,stock,star} = props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className='product'>
@@ -17,6 +18,13 @@ const Product = (props) => {
                 <p>By {seller}</p>
                 <p>$ {price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star"
+                    fullSymbol="fas fa-star rating-icon"
+                    readonly
+                    />
+                    <br />
                 <button onClick={()=>props.handlerAddToCart(props.product)} 
                 className='btn-regular'
                 >{cartIcon}add to cart</button>
